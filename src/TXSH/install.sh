9ec8781702c0dfc2ea442c8be3eb4ee5004cdc0b
temptechx32 <temptechx@gmail.com>
2026-05-08 23:01:04 +0200

    TXSH IS BORN

---
+++ src/TXSH/install.sh
@@ -0,0 +1,11 @@
+echo "Please Wait while Its Installing techx-shell"
+git clone https://github.com/temptechx32/fsociety.py.git /usr/local/bin/
+
+sudo rm -rf /usr/local/bin/assets
+sudo rm -rf /usr/local/bin/index.html
+sudo rm -rf /usr/local/bin/install.sh
+sudo rm -rf /usr/local/bin/src/TXSH/install.sh
+
+echo "Install Done"
+techx-shell
+
